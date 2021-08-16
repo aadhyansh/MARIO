a363d7cee33c3a7e85ce75566f31e317d7848b2e
aadhyansh <73935325+aadhyansh@users.noreply.github.com>
2021-08-16 12:45:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
-function preload() {
-	world_start = loadSound("world_start.wav");
-	setSprites();
-	MarioAnimation();
+function setup(){
+canvas=createCanvas(1240,336);
+canvas.parent('canvas');
+intializeInSetup(mario);
 }
-
-function setup() {
-	canvas = createCanvas(1240,336);
-	instializeInSetup(mario);
-}
-
-function draw() {
-	game()
-}
-
 
 
 
